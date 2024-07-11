bc0c192df3b71d8fe995168503a431bb921a2ffa
SriramYagnaPriya <sriramyagnapriya2005@gmail.com>
2024-07-11 14:31:53 +0530

    meena

--- test/async.js
+++ test/async.js
@@ -3,11 +3,11 @@
 
 
 //how we can perform on async function
-function fetchData(callback){
-    setTimeout(()=>{
-        callback('admin')
-    },1000);
-}
+// function fetchData(callback){
+//     setTimeout(()=>{
+//         callback('admin')
+//     },1000);
+// }
 function MyData(callback){
     setTimeout(()=>{
         callback({id:10001});
