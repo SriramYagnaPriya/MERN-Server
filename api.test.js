3cbae13bf73a88644de042a92659b6ad3002055a
SriramYagnaPriya <sriramyagnapriya2005@gmail.com>
2024-07-11 14:59:03 +0530

    updates

--- api.test.js
+++ api.test.js
@@ -6,7 +6,7 @@ const fetchData = require('./test/api');
 test('GET /users api data',async()=>{
     const data = await fetchData();
     console.log(data);
-    expect(data).toHaveProperty('getAllUsers[]');
+    expect(data).toHaveProperty('getAllUsers[0]');
     data.getAllUsers.forEach(user=>{
         expect(user).toHaveProperty('_id');
     })
