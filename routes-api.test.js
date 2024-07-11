f849525a09307b85067f0fa811a808f116eeec57
SriramYagnaPriya <sriramyagnapriya2005@gmail.com>
2024-07-11 14:02:10 +0530

    meena

--- routes-api.test.js
+++ routes-api.test.js
@@ -1,18 +1,16 @@
-//routes-api.test.js
-//jest rest 
-//well teat if it contains all the parts or not
 const app = require('./index');
-const request = require('supertest');
-
+const request =require('supertest');
 describe('GET /users ',()=>{
-    test('GET users from /user ap1',async()=>{
-        const res=await  request(app)
+    test('Get users from /user api',async()=>{
+        const res = await request(app)
         .get('/users')
         .expect(200);
         console.log(res.body.getAllUsers[0]);
-        const data = res.body;
+        const data=res.body;
         data.getAllUsers.forEach(user=>{
             expect(user).toHaveProperty('_id');
-        });
+
+        })
     })
+
 })
